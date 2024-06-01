5da653eddc2e4998246dd09c2dc846310147ba0a
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-01 10:11:08 +0300

    Added The Current Pages To The Routing System

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoadingComponent } from './pages/loading/loading.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'loading' },
+  { path: 'loading', component: LoadingComponent },
+  { path: 'timeline', component: HomeComponent },
+  { path: 'auth', component: AuthComponent },
 ];
