c3c198360498bdf6bf17f78e0ecaba01099e39ec
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-06 17:22:58 +0300

    Added The Auth Forms To The Form Component

--- src/app/components/auth/form/form.component.ts
+++ src/app/components/auth/form/form.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule, LoginComponent, RegisterComponent],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrl: './form.component.scss',
 })
-export class FormComponent {
-
-}
+export class FormComponent {}
