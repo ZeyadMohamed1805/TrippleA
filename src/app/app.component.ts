3353be11f09629f845eedc4980b67724007964f6
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-09 12:05:00 +0300

    Completed Adding The Header To The Required Paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layouts/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  isLayoutHidden: boolean = false;
+  routesThatHideLayout: string[] = ['/', '/loading', '/auth'];
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((route: any) => {
+      this.isLayoutHidden = this.routesThatHideLayout.includes(route.url);
+    });
+  }
+}
