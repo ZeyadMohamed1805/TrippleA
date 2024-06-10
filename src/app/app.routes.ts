97192859b9b9c7c3123119af3de37a32d2adb6dd
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-11 01:59:14 +0300

    Added The Navigation Pages To The Routing System

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { LoadingComponent } from './pages/loading/loading.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { TimelineComponent } from './pages/timeline/timeline.component';
 import { SearchComponent } from './pages/search/search.component';
+import { BookmarksComponent } from './pages/bookmarks/bookmarks.component';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'loading' },
@@ -10,4 +13,7 @@ export const routes: Routes = [
   { path: 'loading', component: LoadingComponent },
   { path: 'timeline', component: TimelineComponent },
   { path: 'search', component: SearchComponent },
+  { path: 'bookmarks', component: BookmarksComponent },
+  { path: 'notifications', component: NotificationsComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
