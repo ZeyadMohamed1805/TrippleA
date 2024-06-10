d6c9a2117186b8fd1b4c2ae6fa57f5cfd6857cb4
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-10 20:27:21 +0300

    Updated Auth Form Components' Button Style

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -7,20 +7,13 @@ import {
 } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { NgIf } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    NgIf,
-  ],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, NgIf],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
