06d88fdcdab7530038090ea97584c81ebbcec5c8
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-11 11:27:11 +0300

    Added Notifications Badge

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { AvatarComponent } from '../../components/common/avatar/avatar.component';
 import { Router, RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AvatarComponent, RouterLink],
+  imports: [AvatarComponent, RouterLink, MatBadgeModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -14,7 +16,6 @@ export class HeaderComponent {
     { icon: '../../../assets/icons/timeline.svg', path: '/timeline' },
     { icon: '../../../assets/icons/search.svg', path: '/search' },
     { icon: '../../../assets/icons/bookmark.svg', path: '/bookmarks' },
-    { icon: '../../../assets/icons/notification.svg', path: '/notifications' },
   ];
 
   constructor(private router: Router) {}
