7e8532bff6d59af66c2975502f4a12d1fab599b5
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-15 00:40:58 +0300

    Completed The Search Page

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FiltersComponent } from '../../components/search/filters/filters.component';
+import { ResultsComponent } from '../../components/search/results/results.component';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FiltersComponent],
+  imports: [FiltersComponent, ResultsComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
