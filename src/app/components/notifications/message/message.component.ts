f07f029106adb7ce77d0eadd5b325b6c040962c8
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-16 00:03:49 +0300

    Completed The Notifications Page

--- src/app/components/notifications/message/message.component.ts
+++ src/app/components/notifications/message/message.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AvatarComponent } from '../../common/avatar/avatar.component';
+import { DatePipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-message',
   standalone: true,
-  imports: [],
+  imports: [AvatarComponent, NgClass, DatePipe],
   templateUrl: './message.component.html',
-  styleUrl: './message.component.scss'
+  styleUrl: './message.component.scss',
 })
 export class MessageComponent {
-
+  @Input({ required: true }) isRead: boolean | undefined;
+  date: number = Date.now();
 }
