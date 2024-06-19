4d616d1332c06ae5ab3e1530a603a2c586e878ad
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-19 22:49:54 +0300

    Completed The Answer Component's Template

--- src/app/components/question/answer/answer.component.ts
+++ src/app/components/question/answer/answer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { AvatarComponent } from '../../common/avatar/avatar.component';
 
 @Component({
   selector: 'app-answer',
   standalone: true,
-  imports: [],
+  imports: [AvatarComponent],
   templateUrl: './answer.component.html',
-  styleUrl: './answer.component.scss'
+  styleUrl: './answer.component.scss',
 })
 export class AnswerComponent {
-
+  actions: string[] = ['Upvote', 'Comment', 'Downvote'];
 }
--- src/app/pages/question/question.component.ts
+++ src/app/pages/question/question.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { DetailsComponent } from '../../components/question/details/details.component';
 import { MatDivider } from '@angular/material/divider';
+import { AnswerComponent } from '../../components/question/answer/answer.component';
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [DetailsComponent, MatDivider],
+  imports: [DetailsComponent, AnswerComponent, MatDivider],
   templateUrl: './question.component.html',
   styleUrl: './question.component.scss',
 })
