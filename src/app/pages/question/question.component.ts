67c5efb252d6cb870cb491ac02e5137d3873ac90
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-19 21:39:54 +0300

    Initialized The Question Page's Answer Component

--- src/app/pages/question/question.component.ts
+++ src/app/pages/question/question.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DetailsComponent } from '../../components/question/details/details.component';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [DetailsComponent],
+  imports: [DetailsComponent, MatDivider],
   templateUrl: './question.component.html',
   styleUrl: './question.component.scss',
 })
