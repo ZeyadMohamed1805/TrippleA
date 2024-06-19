e2e685e942221fbc126979fe0bd44442f8c8650c
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-19 23:30:52 +0300

    Completed The Question Page's Comment Component

--- src/app/components/question/answer/answer.component.ts
+++ src/app/components/question/answer/answer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AvatarComponent } from '../../common/avatar/avatar.component';
+import { CommentComponent } from '../comment/comment.component';
 
 @Component({
   selector: 'app-answer',
   standalone: true,
-  imports: [AvatarComponent],
+  imports: [AvatarComponent, CommentComponent],
   templateUrl: './answer.component.html',
   styleUrl: './answer.component.scss',
 })
--- src/app/components/question/comment/comment.component.ts
+++ src/app/components/question/comment/comment.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { AvatarComponent } from '../../common/avatar/avatar.component';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [],
+  imports: [AvatarComponent, MatDivider],
   templateUrl: './comment.component.html',
-  styleUrl: './comment.component.scss'
+  styleUrl: './comment.component.scss',
 })
-export class CommentComponent {
-
-}
+export class CommentComponent {}
