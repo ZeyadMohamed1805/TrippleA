43cd6df5eebb8763b0f7e8dbc444bb82f37b7712
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-21 01:06:09 +0300

    Completed The Auth Service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private api: ApiService) {}
 
-  constructor() { }
+  login(user: any) {
+    return this.api.post<any, any>('/authentication/signin', user);
+  }
+
+  register(user: any) {
+    return this.api.post<any, any>('/authentication/signup', user);
+  }
 }
---
+++ src/app/types/data/register.ts
@@ -0,0 +1,6 @@
+export type TRegisterData = {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+};
