7d4af894f504022188f6896f5787d641576d176c
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-20 03:01:28 +0300

    Completed The Answer Actions

--- src/app/pages/question/question.component.ts
+++ src/app/pages/question/question.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { DetailsComponent } from '../../components/question/details/details.component';
 import { MatDivider } from '@angular/material/divider';
 import { AnswerComponent } from '../../components/question/answer/answer.component';
+import { AvatarComponent } from '../../components/common/avatar/avatar.component';
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [DetailsComponent, AnswerComponent, MatDivider],
+  imports: [AvatarComponent, DetailsComponent, AnswerComponent, MatDivider],
   templateUrl: './question.component.html',
   styleUrl: './question.component.scss',
 })
-export class QuestionComponent {}
+export class QuestionComponent {
+  actions: string[] = ['Answer', 'Image', 'Reset'];
+}
