1ed509374c80a5203c71b2581ac1c20157727ade
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-22 18:56:18 +0300

    Completed The Token Interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,18 +3,23 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import {
   QueryClient,
   provideAngularQuery,
 } from '@tanstack/angular-query-experimental';
+import { tokenInterceptor } from './middlewares/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withInterceptors([tokenInterceptor]), withFetch()),
     provideAngularQuery(new QueryClient()),
   ],
 };
