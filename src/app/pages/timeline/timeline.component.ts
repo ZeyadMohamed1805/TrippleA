390cda62f14e1123284ee6c20c8283f5363724d0
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-22 19:48:04 +0300

    Completed Fetching The Paginated Questions

--- src/app/pages/timeline/timeline.component.ts
+++ src/app/pages/timeline/timeline.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AskComponent } from '../../components/timeline/ask/ask.component';
 import { QuestionComponent } from '../../components/common/question/question.component';
+import { QuestionService } from '../../services/question/question.service';
 
 @Component({
   selector: 'app-timeline',
@@ -9,4 +10,10 @@ import { QuestionComponent } from '../../components/common/question/question.com
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss',
 })
-export class TimelineComponent {}
+export class TimelineComponent implements OnInit {
+  constructor(public questionService: QuestionService) {}
+
+  ngOnInit(): void {
+    this.questionService.getQuestions();
+  }
+}
--- src/app/services/question/question.service.ts
+++ src/app/services/question/question.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import { injectInfiniteQuery } from '@tanstack/angular-query-experimental';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuestionService {
+  questions: any[] = [];
+  pageSize: number = 2;
+  currentPage: number = 1;
+  totalPages: number = 1;
 
-  constructor() { }
+  constructor(private readonly apiService: ApiService) {}
+
+  query = injectInfiniteQuery(() => ({
+    queryKey: ['questions'],
+    queryFn: async ({ pageParam }) =>
+      pageParam <= this.totalPages &&
+      this.apiService
+        .get(
+          `/GetQuestionsPaginated?PageNumber=${pageParam}&PageSize=${this.pageSize}`
+        )
+        .subscribe({
+          next: (response: any) => {
+            this.questions = [...this.questions, ...response.data.data];
+            this.totalPages = response.data.totalPages;
+            this.pageSize = response.data.pageSize;
+            this.currentPage = response.data.currentPage;
+            console.log(this.questions);
+          },
+          error: (error) => console.log(error),
+        }),
+    initialPageParam: this.currentPage,
+    getPreviousPageParam: () => this.currentPage - 1,
+    getNextPageParam: () => this.currentPage + 1,
+    maxPages: this.totalPages,
+  }));
+
+  getQuestions() {
+    this.query.data();
+  }
+
+  getNextPage() {
+    this.query.fetchNextPage();
+  }
 }
