cc595f531fa83ce5c60b1b6f9f9c44f3940cc299
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-28 15:16:01 +0300

    Completed The Answer Component Data

--- src/app/components/question/answer/answer.component.ts
+++ src/app/components/question/answer/answer.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AvatarComponent } from '../../common/avatar/avatar.component';
 import { CommentComponent } from '../comment/comment.component';
+import { TAnswer } from '../../../types/data/answer';
+import { DatePipe, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-answer',
   standalone: true,
-  imports: [AvatarComponent, CommentComponent],
+  imports: [AvatarComponent, CommentComponent, NgStyle, DatePipe],
   templateUrl: './answer.component.html',
   styleUrl: './answer.component.scss',
 })
 export class AnswerComponent {
   actions: string[] = ['Upvote', 'Comment', 'Downvote'];
+  @Input() answer: TAnswer | undefined;
 }
