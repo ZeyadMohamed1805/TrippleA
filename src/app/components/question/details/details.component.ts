272905a100a580e86b1a0a054e7df44d7ecda47f
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-28 14:51:44 +0300

    Completed The Question Details

--- src/app/components/question/details/details.component.ts
+++ src/app/components/question/details/details.component.ts
@@ -1,33 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AvatarComponent } from '../../common/avatar/avatar.component';
-import { QuestionService } from '../../../services/question/question.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { TPaginatedQuestion } from '../../../types/data/question';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [AvatarComponent],
+  imports: [AvatarComponent, DatePipe],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
 })
-export class DetailsComponent implements OnInit {
-  @Input() questionId: number | undefined;
-
-  constructor(
-    private readonly questionService: QuestionService,
-    private readonly snackBar: MatSnackBar
-  ) {}
-
-  ngOnInit(): void {
-    this.questionService.getQuestion(this.questionId!).subscribe({
-      next: (response) => {
-        console.log(response);
-      },
-      error: () => {
-        this.snackBar.open('Please try again', 'Ok!', {
-          panelClass: ['error-snackbar'],
-        });
-      },
-    });
-  }
+export class DetailsComponent {
+  @Input() question: TPaginatedQuestion | undefined;
 }
