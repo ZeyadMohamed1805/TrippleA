14d32c8692070e0329ce785e57ada315809b2340
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-29 23:04:32 +0300

    Initialized The Response Component

--- src/app/components/question/details/details.component.ts
+++ src/app/components/question/details/details.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { AvatarComponent } from '../../common/avatar/avatar.component';
 import { TPaginatedQuestion } from '../../../types/data/question';
 import { DatePipe } from '@angular/common';
+import { UserService } from '../../../services/user/user.service';
 
 @Component({
   selector: 'app-details',
@@ -10,6 +11,21 @@ import { DatePipe } from '@angular/common';
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnChanges {
   @Input() question: TPaginatedQuestion | undefined;
+  userName: string | undefined;
+
+  constructor(private readonly userService: UserService) {}
+
+  ngOnChanges(): void {
+    console.log(this.question);
+
+    this.userService.getUser(this.question?.userId!).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.userName = response.data.userName;
+      },
+      error: (error) => console.log(error),
+    });
+  }
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../api/api.service';
 import { TUser } from '../../types/data/user';
+import { TResponse } from '../../types/data/response';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,6 @@ export class UserService {
   constructor(private readonly apiService: ApiService) {}
 
   getUser(userId: string) {
-    return this.apiService.get<TUser>(`/${userId}`);
+    return this.apiService.get<TResponse<TUser>>(`/${userId}`);
   }
 }
