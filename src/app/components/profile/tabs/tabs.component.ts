e701f1d2edc3f777d17c63763632b4f86f6386c8
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-30 12:51:51 +0300

    Initialized The Bookmarks Service

--- src/app/components/profile/tabs/tabs.component.ts
+++ src/app/components/profile/tabs/tabs.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { BookmarksComponent } from '../bookmarks/bookmarks.component';
 
 @Component({
   selector: 'app-tabs',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, BookmarksComponent],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.scss',
 })
