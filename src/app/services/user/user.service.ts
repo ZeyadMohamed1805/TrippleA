2d8495f9115b96f2fc2c5158fd1f7b0748bcd7e1
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-07-01 01:39:29 +0300

    Completed The User Profile Service

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../api/api.service';
-import { TUser } from '../../types/data/user';
+import { TUser, TUserProfile } from '../../types/data/user';
 import { TResponse } from '../../types/data/response';
+import { TokenService } from '../token/token.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private readonly apiService: ApiService) {}
+  userProfile: TUserProfile | undefined;
+
+  constructor(
+    private readonly apiService: ApiService,
+    private readonly tokenService: TokenService
+  ) {}
 
   getUser(userId: string) {
     return this.apiService.get<TResponse<TUser>>(`/${userId}`);
+  }
+
+  getUserProfile(userId: string) {
+    this.apiService
+      .get<TResponse<TUserProfile>>(`/userprofile/${userId}`)
+      .subscribe({
+        next: (response) => (this.userProfile = response.data),
+        error: (error) => console.log(error),
+      });
   }
 }
--- src/app/types/data/user.ts
+++ src/app/types/data/user.ts
@@ -3,3 +3,27 @@ export type TUser = {
   votes: number;
   userName: string;
 };
+
+export type TUserProfile = {
+  id: string;
+  votes: number;
+  userName: string;
+  userProfileAnswers: TUserProfileAnswer[];
+  userProfileQuestions: TUserProfileQuestion[];
+};
+
+export type TUserProfileAnswer = {
+  answerId: number;
+  questionId: number;
+  votes: number;
+  questionTitle: string;
+  answerContent: string;
+};
+
+export type TUserProfileQuestion = {
+  id: number;
+  description: string;
+  title: string;
+  image: string;
+  createdIn: string;
+};
