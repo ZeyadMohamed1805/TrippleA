79586e5b1c46392cd381db42b691469deb2ead97
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-30 21:37:40 +0300

    Completed The Delete Comment

--- src/app/components/question/delete/delete.component.ts
+++ src/app/components/question/delete/delete.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { CommentService } from '../../../services/comment/comment.service';
 
 @Component({
   selector: 'app-delete',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule, MatButtonModule],
   templateUrl: './delete.component.html',
-  styleUrl: './delete.component.scss'
+  styleUrl: './delete.component.scss',
+  providers: [CommentService],
 })
 export class DeleteComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public commentId: number,
+    private readonly commentService: CommentService
+  ) {}
 
+  onConfirm() {
+    this.commentService.deleteComment(this.commentId);
+  }
 }
