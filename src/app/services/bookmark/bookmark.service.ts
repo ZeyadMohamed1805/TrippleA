30cfdf8aab65fff35b01edf996f57822264da63e
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-30 13:06:24 +0300

    Completed Adding The Bookmarks

--- src/app/services/bookmark/bookmark.service.ts
+++ src/app/services/bookmark/bookmark.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
 import { StorageService } from '../storage/storage.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookmarkService {
-  constructor(private readonly storageService: StorageService) {}
+  constructor(
+    private readonly storageService: StorageService,
+    private readonly snackBar: MatSnackBar
+  ) {}
 
   get() {
     return this.storageService.getItem('AAA_BOOKMARKS');
   }
 
-  set(bookmark: any) {
+  set(bookmark: number) {
     const bookmarks: any = this.get();
     this.storageService.setItem(
       'AAA_BOOKMARKS',
-      bookmarks ? [...bookmarks, bookmark] : bookmark
+      bookmarks ? [...bookmarks, bookmark] : [bookmark]
     );
+    this.snackBar.open('Question Added To Bookmark!', 'Awesome!');
   }
 
   remove() {
