4087620218d1e474e866aac2786055191807f8ef
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-07-01 03:25:23 +0300

    Completed The Profile Page's Questions Component

--- src/app/components/profile/tabs/tabs.component.ts
+++ src/app/components/profile/tabs/tabs.component.ts
@@ -4,11 +4,19 @@ import { BookmarksComponent } from '../bookmarks/bookmarks.component';
 import { UserService } from '../../../services/user/user.service';
 import { TokenService } from '../../../services/token/token.service';
 import { NgIf } from '@angular/common';
+import { QuestionsComponent } from '../questions/questions.component';
+import { AnswersComponent } from '../answers/answers.component';
 
 @Component({
   selector: 'app-tabs',
   standalone: true,
-  imports: [MatTabsModule, BookmarksComponent, NgIf],
+  imports: [
+    MatTabsModule,
+    BookmarksComponent,
+    QuestionsComponent,
+    AnswersComponent,
+    NgIf,
+  ],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.scss',
 })
