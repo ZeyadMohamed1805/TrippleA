2076a45e3e82c2c97e92fbaccf4a4a72264d0585
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-07-01 11:20:29 +0300

    Completed The Admin Page's Design

--- src/app/components/admin/tabs/tabs.component.ts
+++ src/app/components/admin/tabs/tabs.component.ts
@@ -17,16 +17,12 @@ export class TabsComponent {
   ads: any[] = [
     {
       id: 1,
-      image:
-        'http://res.cloudinary.com/dzlsdjx13/image/upload/v1719749725/knucu8tdbunbfkmqgtsc.svg',
       title: 'ShopFusion',
       companyLink: 'https://shopfusion-project.vercel.app',
       companyName: 'ShopFusion',
     },
     {
       id: 2,
-      image:
-        'http://res.cloudinary.com/dzlsdjx13/image/upload/v1719749827/iksviciurrkspi8uxmqg.png',
       title: 'ZCommerce',
       companyLink: 'https://z-commerce-project.vercel.app',
       companyName: 'ZCommerce',
@@ -37,22 +33,22 @@ export class TabsComponent {
       id: 26,
       description: 'Description',
       title: 'Title',
-      image:
-        'http://res.cloudinary.com/dzlsdjx13/image/upload/v1719722923/otj2ecsxq9rdt2ygqdh6.png',
-      createdIn: '2024-06-30T07:48:40.3167348',
-      categoryName: 'test',
-      userName: 'Hamada_1805',
+      categoryid: 'test',
     },
   ];
   answers: any[] = [
     {
       id: 6248,
-      votes: 0,
-      createdIn: '2024-06-30T11:05:28.9998348',
-      image: 'NoFile',
       description: 'This is my first answer',
       questionId: 26,
-      userId: '335544a9-59c1-42a4-9dcd-cb05d9fb2491',
+    },
+  ];
+  users: any[] = [
+    {
+      email: 'email',
+      password: 'password',
+      userName: 'userName',
+      name: 'name',
     },
   ];
 }
