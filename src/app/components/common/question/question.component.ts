60b73ec049e71f0c6f1c362929d330fc525b7ac9
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-07-02 02:23:16 +0300

    Completed The Share Feature

--- src/app/components/common/question/question.component.ts
+++ src/app/components/common/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
 import { AvatarComponent } from '../avatar/avatar.component';
 import { Router } from '@angular/router';
 import { TQuestion } from '../../../types/data/question';
@@ -6,11 +7,13 @@ import { DatePipe, NgStyle } from '@angular/common';
 import { TAvatar } from '../../../types/data/avatar';
 import { MatIcon } from '@angular/material/icon';
 import { BookmarkService } from '../../../services/bookmark/bookmark.service';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [AvatarComponent, MatIcon, NgStyle, DatePipe],
+  imports: [AvatarComponent, MatIcon, MatMenuModule, NgStyle, DatePipe],
   templateUrl: './question.component.html',
   styleUrl: './question.component.scss',
 })
@@ -22,7 +25,9 @@ export class QuestionComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private readonly bookmarkService: BookmarkService
+    private readonly bookmarkService: BookmarkService,
+    private clipboard: Clipboard,
+    private snackBar: MatSnackBar
   ) {}
 
   onDetailsClick(): void {
@@ -40,5 +45,13 @@ export class QuestionComponent implements OnInit {
       userName: this.data?.userName || '',
       createdIn: this.data?.createdIn,
     };
+  }
+
+  copyLink() {
+    this.data &&
+      this.clipboard.copy(`http://localhost:4200/question/${this.data.id}`);
+    this.snackBar.open('Link copied to clipboard!', 'Close', {
+      duration: 2000,
+    });
   }
 }
