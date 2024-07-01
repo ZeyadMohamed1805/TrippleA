eed0a6d1e30086aa2fdfe61846dbea93799ae665
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-07-02 00:41:28 +0300

    Completed Admin Login

--- src/app/pages/loading/loading.component.ts
+++ src/app/pages/loading/loading.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component } from '@angular/core';
 import { SpinnerComponent } from '../../components/loading/spinner/spinner.component';
 import { Router } from '@angular/router';
 import { StorageService } from '../../services/storage/storage.service';
+import { TokenService } from '../../services/token/token.service';
 
 @Component({
   selector: 'app-loading',
@@ -11,12 +12,19 @@ import { StorageService } from '../../services/storage/storage.service';
   styleUrl: './loading.component.scss',
 })
 export class LoadingComponent implements AfterViewInit {
-  constructor(private router: Router, private storageService: StorageService) {}
+  constructor(
+    private router: Router,
+    private storageService: StorageService,
+    private tokenService: TokenService
+  ) {}
 
   ngAfterViewInit(): void {
+    const role = this.tokenService.getRole();
     setTimeout(() => {
       this.storageService.getItem<string>('AAA_TOKEN')
-        ? this.router.navigateByUrl('timeline')
+        ? role === 'Admin'
+          ? this.router.navigateByUrl('admin')
+          : this.router.navigateByUrl('timeline')
         : this.router.navigateByUrl('auth');
     }, 3000);
   }
